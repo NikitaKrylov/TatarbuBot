0c2167fac32f83e93cbe858e61dbbdb9d5be7dbd
Kristina <kiegorova04@mail.ru>
2024-05-13 16:05:34 +0300

    started lessonBlock

--- frontend/src/components/home page/current lesson/LessonBlock.jsx
+++ frontend/src/components/home page/current lesson/LessonBlock.jsx
@@ -1,7 +1,21 @@
+import styles from './LessonBlock.module.scss'
+
 export default function LessonBlock(){
+    let nameCourse = 'Базовый курс, первый модуль'
+    let numLesson = 3;
+    let nameLesson = 'Название урока';
+
     return(
         <>
-            <section>lesson</section>
+            <section className={styles.section}>
+                <h2 className={styles.h2}>{nameCourse}</h2>
+                <div className={styles.div}>
+                    <h3 className={styles.h3}>
+                        <p className={styles.p}>Урок {numLesson}</p> 
+                        <p className={styles.p}>{nameLesson}</p>
+                    </h3>
+                </div>
+            </section>
         </>
     )
 }

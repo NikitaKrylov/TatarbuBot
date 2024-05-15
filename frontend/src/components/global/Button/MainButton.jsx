ba462d822346e89208ae4a1fbe50f656f98032b6
Osokin Yaroslav <55447590+osyaros@users.noreply.github.com>
2024-05-15 18:31:18 +0300

    Delete frontend directory

--- frontend/src/components/global/Button/MainButton.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './MainButton.module.scss'
-
-export default function MainButton ({text}){
-    return(
-        <button className={styles.button}>
-            {text}
-        </button>
-    )
-}
